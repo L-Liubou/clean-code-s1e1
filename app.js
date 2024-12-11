6f3968be20dd3dfb97b325d5d3573ee97a2f5383
Liubou Levitskaya <lyubasaraeva@gmail.com>
2024-12-12 02:40:35 +0300

    feat(no-rule): restore ability to edit tasks in TODO list

--- app.js
+++ app.js
@@ -69,34 +69,28 @@ addButton.addEventListener('click', () => {
   addTask();
 });
 
-//Edit an existing task.
+// Edit an existing task.
+function editTask() {
+  console.log('Edit Task...');
+  console.log('Change "edit" to "save"');
 
-var editTask=function(){
-    console.log("Edit Task...");
-    console.log("Change 'edit' to 'save'");
+  const listItem = this.parentNode;
+  const editInput = listItem.querySelector('.task__input');
+  const label = listItem.querySelector('.task__label');
+  const editBtn = listItem.querySelector('.task__btn--edit');
+  const containsClass = listItem.classList.contains('task--edit-mode');
 
+  // If class of the parent is task--edit-mode
+  if (containsClass) {
+    label.innerText = editInput.value;
+    editBtn.innerText = 'Edit';
+  } else {
+    editInput.value = label.innerText;
+    editBtn.innerText = 'Save';
+  }
 
-    var listItem=this.parentNode;
-
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
-    if(containsClass){
-
-        //switch to .editmode
-        //label becomes the inputs value.
-        label.innerText=editInput.value;
-        editBtn.innerText="Edit";
-    }else{
-        editInput.value=label.innerText;
-        editBtn.innerText="Save";
-    }
-
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
-};
+  listItem.classList.toggle('task--edit-mode');
+}
 
 
 //Delete task.
@@ -134,20 +128,19 @@ var taskIncomplete=function(){
 }
 
 
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
-    console.log("bind list item events");
-//select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+function bindTaskEvents(taskListItem, checkBoxEventHandler) {
+  console.log("bind list item events");
+  //select ListItems children
+  var checkBox=taskListItem.querySelector("input[type=checkbox]");
+  const editButton = taskListItem.querySelector('.task__btn--edit');
+  var deleteButton=taskListItem.querySelector("button.delete");
 
-
-    //Bind editTask to edit button.
-    editButton.onclick=editTask;
-    //Bind deleteTask to delete button.
-    deleteButton.onclick=deleteTask;
-    //Bind taskCompleted to checkBoxEventHandler.
-    checkBox.onchange=checkBoxEventHandler;
+  //Bind editTask to edit button.
+  editButton.onclick=editTask;
+  //Bind deleteTask to delete button.
+  deleteButton.onclick=deleteTask;
+  //Bind taskCompleted to checkBoxEventHandler.
+  checkBox.onchange=checkBoxEventHandler;
 }
 
 //cycle over incompleteTaskHolder ul list items
